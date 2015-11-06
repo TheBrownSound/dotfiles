bd9b1c10f3f78276a69131a6311711bf093be3b9
Jarad Comingdeer <jarad@jcomingdeer.com>
2015-11-06 08:45:49 -0800

    Add tmux config

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/tmux/.tmux.conf" ~
 
 if [ "$(uname)" == "Darwin" ]; then
   ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
